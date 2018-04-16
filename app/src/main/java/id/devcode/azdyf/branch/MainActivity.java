91d2a237b47b9bcc6247015de00e8e199cc96be5
azdyf <azdyf.fm@gmail.com>
2018-04-16 10:51:47 +0700

    Updated MainActivity

--- app/src/main/java/id/devcode/azdyf/branch/MainActivity.java
+++ app/src/main/java/id/devcode/azdyf/branch/MainActivity.java
@@ -52,14 +52,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void doSomeWork() {
 
-        int i = 10;
-        int ii = 50;
+        int i = 20;
+        int ii = 70;
 
         String iString = Integer.toString(i);
         String iiString;
 
         if (i <= 100) {
-            iiString = Integer.toString(ii - 9);
+            iiString = Integer.toString(ii + 9);
         }
     }
 }
